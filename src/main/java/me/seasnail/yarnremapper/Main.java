dc5df4edf552a04064f3882c611a2ac379f7c88b
seasnail8169 <seasnail@meteorclient.com>
2021-08-08 20:58:55 +0100

    Rewrote file management and caching and removed bad remapping functions

--- src/main/java/me/seasnail/yarnremapper/Main.java
+++ src/main/java/me/seasnail/yarnremapper/Main.java
@@ -1,6 +1,6 @@
 package me.seasnail.yarnremapper;
 
-import com.google.common.net.UrlEscapers;
+import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import net.fabricmc.mapping.reader.v2.MappingGetter;
 import net.fabricmc.mapping.reader.v2.TinyMetadata;
@@ -9,213 +9,170 @@ import net.fabricmc.mapping.reader.v2.TinyVisitor;
 import org.apache.commons.io.FileUtils;
 
 import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
-import java.net.URLConnection;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Main {
-    private static final String[] PATTERNS = new String[]{
-            "(net(\\.|\\/)minecraft(\\.|\\/))([a-z]+)?([a-ln-z]{5,}_\\d+|\\.|\\\\|\\$)+(?<!\\.|\\\\|[^0-9])",
-            "class_\\d+",
-            "method_\\d+",
-            "field_\\d+"
-    };
+    private static final File DIR = new File(System.getProperty("user.home"), ".yarn-remapper");
 
-    private static Map<String, String> mappings = null;
+    private static Map<String, String> MAPPINGS;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException {
         // Read input info
-        System.out.println("Please input the path to the file/folder you would like to remap.");
+        System.out.println("Please input the path to the remap jar.");
 
         String path = new BufferedReader(new InputStreamReader(System.in)).readLine();
-        File TARGET_FILE = new File(path);
+        File target = new File(path);
 
-        if (!TARGET_FILE.exists()) {
-            System.out.println("Could not locate such a file.");
+        if (!target.exists()) {
+            System.out.println("That file does not exist.");
             System.exit(0);
         }
 
-        if (!TARGET_FILE.isDirectory() && !TARGET_FILE.getName().endsWith(".java")) {
-            System.out.println("Invalid path or file type inputted (must be a directory, java source file or java class).");
+        if (!target.getName().endsWith(".jar")) {
+            System.out.println("Invalid file input (must be a jar file).");
             System.exit(0);
         }
-
-        String mcVersion = "", yarnVersion = "";
 
         System.out.println("Please input the Minecraft version you wish to remap to.");
 
-        // Fetch yarn version
-        try {
-            mcVersion = new BufferedReader(new InputStreamReader(System.in)).readLine();
+        String mcVersion = new BufferedReader(new InputStreamReader(System.in)).readLine();
+        int latestYarnBuild = fetchLatestYarnBuild(mcVersion);
 
-            URL url = new URL("https://meta.fabricmc.net/v2/versions/yarn/" + mcVersion);
-            URLConnection request = url.openConnection();
-            request.connect();
+        File mcVersionFolder = new File(DIR, mcVersion);
+        if (mcVersionFolder.exists()) {
+            int latestBuild = 0;
 
-            YarnVersion[] versions = new Gson().fromJson(new InputStreamReader((InputStream) request.getContent()), YarnVersion[].class);
-            Optional<YarnVersion> yarnVer = Arrays.stream(versions).max(Comparator.comparingInt(v -> v.build));
+            for (File file : mcVersionFolder.listFiles()) {
+                if (file.getName().endsWith(".tiny")) {
+                    int build = Integer.parseInt(file.getName().substring(0, 1));
 
-            if (yarnVer.isPresent()) yarnVersion = yarnVer.get().version;
-        } catch (IOException e) {
-            System.out.println("Invalid Minecraft version input.");
-            System.exit(0);
+                    if (build > latestBuild) latestBuild = build;
+                }
+            }
+
+            if (latestBuild > latestYarnBuild) latestYarnBuild = latestBuild;
         }
 
-        if (mcVersion.equals("") || yarnVersion.equals("")) {
-            System.out.println("Invalid Minecraft version input.");
+        fetchLatestMappings(mcVersion, latestYarnBuild);
+
+        File mappingsFile = new File(mcVersionFolder, latestYarnBuild + ".tiny");
+        if (!mappingsFile.exists()) {
+            System.out.println("Failed to fetch mappings!");
             System.exit(0);
         }
-
-        // Download yarn mappings
-        System.out.println("Downloading yarn " + yarnVersion + " mappings…");
-
-        File jarFile = new File(TARGET_FILE.getParentFile(), "yarn-mappings.jar");
-
-        try {
-            String encodedYarnVersion = UrlEscapers.urlFragmentEscaper().escape(yarnVersion);
-            String artifactUrl = "https://maven.fabricmc.net/net/fabricmc/yarn/" + encodedYarnVersion + "/yarn-" + encodedYarnVersion + "-v2.jar";
-            FileUtils.copyURLToFile(new URL(artifactUrl), jarFile);
-        } catch (IOException e) {
-            System.out.printf("Failed to fetch yarn mappings of version: %s.", yarnVersion);
-            System.exit(0);
-            return;
-        }
-
-        File MAPPINGS_FILE = new File(TARGET_FILE.getParentFile(), "mappings.tiny");
-
-        try (FileSystem jar = FileSystems.newFileSystem(jarFile.toPath(), (ClassLoader) null)) {
-            Files.copy(jar.getPath("mappings/mappings.tiny"), MAPPINGS_FILE.toPath(), StandardCopyOption.REPLACE_EXISTING);
-        } catch (IOException e) {
-            System.out.println("Failed to extract mappings!");
-            System.exit(0);
-            return;
-        }
-
-        jarFile.deleteOnExit();
-        MAPPINGS_FILE.deleteOnExit();
 
         // Load mappings
-        Map<String, String> mappings = new HashMap<>();
+        Main.MAPPINGS = readMappings(mappingsFile);
+        System.out.println("Successfully loaded mappings.");
+    }
 
-        try (BufferedReader mappingReader = Files.newBufferedReader(MAPPINGS_FILE.toPath())) {
-            TinyV2Factory.visit(mappingReader, new TinyVisitor() {
-                private final Map<String, Integer> namespaceStringToColumn = new HashMap<>();
+    public static int fetchLatestYarnBuild(String mcVersion) throws IOException, InterruptedException, URISyntaxException {
+        HttpRequest.Builder request = HttpRequest.newBuilder()
+                .uri(new URI("https://meta.fabricmc.net/v2/versions/yarn/" + mcVersion))
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .header("Accept", "application/json");
 
-                private void addMappings(MappingGetter name) {
-                    mappings.put(name.get(namespaceStringToColumn.get("intermediary")).replace('/', '.'),
-                            name.get(namespaceStringToColumn.get("named")).replace('/', '.'));
-                }
+        InputStream res = HttpClient.newHttpClient().send(request.build(), HttpResponse.BodyHandlers.ofInputStream()).body();
 
-                @Override
-                public void start(TinyMetadata metadata) {
-                    namespaceStringToColumn.put("intermediary", metadata.index("intermediary"));
-                    namespaceStringToColumn.put("named", metadata.index("named"));
-                }
+        List<YarnVersion> versions = new Gson().fromJson(new InputStreamReader(res), new TypeToken<List<YarnVersion>>(){}.getType());
 
-                @Override
-                public void pushClass(MappingGetter name) {
-                    addMappings(name);
-                }
+        versions.sort(Comparator.comparingInt(version -> version.build));
+        Collections.reverse(versions);
 
-                @Override
-                public void pushMethod(MappingGetter name, String descriptor) {
-                    addMappings(name);
-                }
+        if (!versions.isEmpty()) return versions.get(0).build;
+        else return 0;
+    }
 
-                @Override
-                public void pushField(MappingGetter name, String descriptor) {
-                    addMappings(name);
-                }
-            });
+    public static void fetchLatestMappings(String mcVersion, int yarnBuild) throws IOException {
+        File mcVersionFolder = new File(DIR, mcVersion);
 
+        if (mcVersionFolder.exists()) FileUtils.cleanDirectory(mcVersionFolder);
+        else mcVersionFolder.mkdir();
+
+        String yarnVersion = mcVersion + "+build." + yarnBuild;
+
+        // Download yarn mappings
+        System.out.printf("Downloading Yarn mappings for Minecraft %s (Build %d)...", mcVersion, yarnBuild);
+
+        File jarFile = new File(mcVersionFolder, "yarn.jar");
+        if (jarFile.exists()) jarFile.delete();
+
+        jarFile.deleteOnExit();
+
+        try {
+            FileUtils.copyURLToFile(new URL("https://maven.fabricmc.net/net/fabricmc/yarn/" + yarnVersion + "/yarn-" + yarnVersion + "-v2.jar"), jarFile);
+            System.out.println("Complete.");
         } catch (IOException e) {
-            System.out.println("Failed to load mappings!");
+            System.out.println("Failed.");
+            System.exit(0);
+            return;
+        }
+
+        // Extract mappings from jar
+        System.out.print("Extracting tiny mappings from Yarn jar...");
+
+        File mappingsFile = new File(mcVersionFolder, yarnBuild + ".tiny");
+
+        try (FileSystem jar = FileSystems.newFileSystem(jarFile.toPath(), (ClassLoader) null)) {
+            Files.copy(jar.getPath("mappings/mappings.tiny"), mappingsFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            System.out.println("Complete.");
+        } catch (IOException e) {
+            System.out.println("Failed");
             System.exit(0);
         }
 
-        Main.mappings = mappings;
-        System.out.println("Successfully loaded mappings.");
-
-        // Remap recursively for all files
-        if (TARGET_FILE.isDirectory()) remapAll(TARGET_FILE);
-        else remapOne(TARGET_FILE);
-
-        System.out.println("Remapping finished, exiting.");
+        jarFile.delete();
     }
 
-    private static void remapAll(File input) {
-        File[] contents = input.listFiles();
+    public static Map<String, String> readMappings(File file) throws IOException {
+        Map<String, String> mappings = new HashMap<>();
 
-        if (contents == null || contents.length < 1) return;
+        TinyV2Factory.visit(Files.newBufferedReader(file.toPath()), new TinyVisitor() {
+            private final Map<String, Integer> namespaceStringToColumn = new HashMap<>();
 
-        for (File file : contents) {
-            if (file.isDirectory()) remapAll(file);
-            else if (file.getName().endsWith(".java")) remapOne(file);
-        }
-    }
+            private void addMappings(MappingGetter name) {
+                mappings.put(
+                        name.get(namespaceStringToColumn.get("intermediary")).replace('/', '.'),
+                        name.get(namespaceStringToColumn.get("named")).replace('/', '.')
+                );
+            }
 
-    private static void remapOne(File input) {
-        String stringified = "";
+            @Override
+            public void start(TinyMetadata metadata) {
+                namespaceStringToColumn.put("intermediary", metadata.index("intermediary"));
+                namespaceStringToColumn.put("named", metadata.index("named"));
+            }
 
-        try {
-            stringified = Files.readString(input.toPath());
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("Could not read " + input.getName());
-        }
+            @Override
+            public void pushClass(MappingGetter name) {
+                addMappings(name);
+            }
 
-        for (String pattern : PATTERNS) {
-            stringified = mapByRegex(pattern, stringified);
-        }
+            @Override
+            public void pushMethod(MappingGetter name, String descriptor) {
+                addMappings(name);
+            }
 
-        try {
-            FileWriter f2 = new FileWriter(input, false);
-            f2.write(stringified);
-            f2.close();
-            System.out.println("Remapped " + input.getName());
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("Failed to write to " + input.getName());
-        }
-    }
+            @Override
+            public void pushField(MappingGetter name, String descriptor) {
+                addMappings(name);
+            }
+        });
 
-
-    private static String mapByRegex(String regex, String stringInput) {
-        if (mappings == null) return stringInput;
-
-        String[] input = {stringInput};
-
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(input[0]);
-
-        while (matcher.find()) {
-            String match = matcher.group(0);
-
-            mappings.forEach((intermediary, named) -> {
-                if (intermediary.contains(match)) {
-                    input[0] = input[0].replace(intermediary, named.replaceAll("\\$", "\\."));
-                    input[0] = input[0].replace(trimClassName(intermediary), trimClassName(named.replaceAll("\\$", "\\.")));
-                }
-            });
-        }
-
-        return input[0];
-    }
-
-    private static String trimClassName(String packagedClassName) {
-        int lastDot = packagedClassName.lastIndexOf('.');
-        if (lastDot != -1) packagedClassName = packagedClassName.substring(lastDot + 1);
-        return packagedClassName.replaceAll("\\$", ".");
+        return mappings;
     }
 
     public static class YarnVersion {
         public int build;
-        public String version;
     }
 }
